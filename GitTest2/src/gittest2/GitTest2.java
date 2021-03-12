6d7deda71c9b2a7af21b109cf81782039868d210
ethan <ethan@192.168.2.121>
2021-03-12 09:36:06 -0500

    Cleaned Up

--- GitTest2/src/gittest2/GitTest2.java
+++ GitTest2/src/gittest2/GitTest2.java
@@ -17,6 +17,11 @@ public class GitTest2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        
     }
     
 }
